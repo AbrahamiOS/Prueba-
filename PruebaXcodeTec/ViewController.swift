4c509b39ff5992b35f6f3347564232ff1c212b44
Abraham Monter Salas <abrahammontersalas@macbook-air-de-abraham.local>
2015-11-08 15:32:28 -0600

    Initial Commit

---
+++ PruebaXcodeTec/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaXcodeTec
+//
+//  Created by Abraham Monter Salas on 08/11/15.
+//  Copyright © 2015 Abraham Monter Salas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
